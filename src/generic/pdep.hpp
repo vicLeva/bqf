4dffc6473e6bebadb87f927545a53977df45c569
Bertrand LE GAL <blegal@users.noreply.github.com>
2023-12-03 16:53:03 +0100

    Removing uint32_t useless functions

--- src/generic/pdep.hpp
+++ src/generic/pdep.hpp
@@ -19,21 +19,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //
-inline int pdep_u32_c(uint32_t val, uint32_t mask)
-{
-  unsigned int res = 0;
-  for (uint32_t bb = 1; mask; bb += bb) {
-    if (val & bb)
-      res |= mask & -mask;
-    mask &= mask - 1;
-  }
-  return res;
-}
-//
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//
 inline int64_t pdep_u64_c(const uint64_t val, uint64_t mask)
 {
   int64_t res = 0;
@@ -50,37 +35,9 @@ inline int64_t pdep_u64_c(const uint64_t val, uint64_t mask)
 //
 //
 #if defined(__SSE4_2__)
-inline int pdep_u32_builtin(uint32_t val, uint32_t mask)
-{
-  return _pdep_u32(val, mask);
-}
-#endif
-//
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//
-#if defined(__SSE4_2__)
 inline int64_t pdep_u64_builtin(const uint64_t val, const uint64_t mask)
 {
   return _pdep_u64(val, mask);
-}
-#endif
-//
-//
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//
-#if defined(__SSE4_2__)
-int pdep_u32_x86(uint32_t num, uint32_t rank)
-{
-    int rsu;
-	  asm("pdep %[mask], %[num], %[rsu]"
-		    : [rsu]  "=r" (rsu)
-        : [num]  "r" (val),
-          [mask] "r" (mask)
-    );
-    return rsu;
 }
 #endif
 //
