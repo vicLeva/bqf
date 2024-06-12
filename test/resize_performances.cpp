2aec452fb5d5c8b67f1dd06f96843451f3377284
NicolasBuchin <nicolas.buchin@etudiant.univ-rennes.fr>
2024-06-12 10:49:24 +0200

    changed random kmer for random hashes in performance test

--- test/resize_performances.cpp
+++ test/resize_performances.cpp
@@ -19,7 +19,6 @@ void mock_resize(Bqf* bqf, int n){
 
     bqf->size_limit = num_quots * 0.95;
 
-    // In machine words
     bqf->number_blocks = std::ceil(num_quots / BLOCK_SIZE);
 
     bqf->filter = std::vector<uint64_t>(num_of_words);
@@ -32,42 +31,28 @@ void mock_resize(Bqf* bqf, int n){
 }
 
 int main() {
-    const uint64_t q = 8;
+    const uint64_t q = 12;
     const uint64_t c = 5;
     const uint64_t k = 32;
     const uint64_t z = 11;
     const uint64_t kmer_size = (k - z);
     Bqf_ec mock = Bqf_ec(q, c, k, z, false);
     Bqf_ec resize = Bqf_ec(q, c, k, z, false);
-
-    // rdm stuff
-    const char alphabet[] = "ACGT";
-    const int alphabetSize = sizeof(alphabet) - 1;
+    
     std::random_device rd;
     std::mt19937 gen(rd()); // random seed
-    std::uniform_int_distribution<> dis(0, alphabetSize - 1);
-    std::stringstream randomKmer;
-    std::string kmer;
+    std::uniform_int_distribution<uint64_t> dis(0ULL, (1ULL << (2 * kmer_size)) - 1ULL);
+    uint64_t hashed_kmer;
 
     std::cout << "Quotient size, Number of inserted elements, Old time, new time" << std::endl;
-
 
     while (resize.remainder_size != resize.count_size) {
         // fill up
         while(resize.elements_inside < resize.size_limit - 1) {
-            // making random kmers
-            for (uint64_t i = 0; i < kmer_size; ++i) {
-                randomKmer << alphabet[dis(gen)];
-            }
-
-            // inserting random kmers
-            kmer = randomKmer.str();
-            resize.insert(kmer, 1);
-            mock.insert(kmer, 1);
-
-            // clears the random Kmer stream
-            randomKmer.str(std::string());
-            randomKmer.clear();
+            // inserting random hashes
+            hashed_kmer = dis(gen);
+            resize.insert(hashed_kmer, 1);
+            mock.insert(hashed_kmer, 1);
         }
 
         // calculating times
@@ -79,24 +64,14 @@ int main() {
         resize.resize(1);
         double resizeTime = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - resizeStart).count();
 
-        if (mock.elements_inside != resize.elements_inside || mock.filter.size() != resize.filter.size()){
-            std::cout << "WTF" << std::endl;
-            exit(1);
-        }
-
-        for (uint i = 0; i < mock.filter.size(); i++){
-            if (resize.filter[i] != mock.filter[i]){
-                std::cout << "ok we have a problem..." << std::endl;
-                exit(1);
-            }
-        }
-
         std::cout << (resize.quotient_size - 1) << ',' << (resize.elements_inside + 1) << ',' << mockTime << ',' << resizeTime << std::endl;
 
-        resize.save_on_disk("./bqfs/rdm_resize_q" + std::to_string(resize.quotient_size));
-        mock.save_on_disk("./bqfs/rdm_mock_q" + std::to_string(mock.quotient_size));
+        
+
+        // resize.save_on_disk("./bqfs/rdm_resize_q" + std::to_string(resize.quotient_size));
+        // mock.save_on_disk("./bqfs/rdm_mock_q" + std::to_string(mock.quotient_size));
     }
-    resize.save_on_disk("./bqfs/rdm_resize_done" + std::to_string(resize.quotient_size));
-    mock.save_on_disk("./bqfs/rdm_mock_done" + std::to_string(mock.quotient_size));
-    std::cout << "All Done ! (wtf)" << std::endl;
+    // resize.save_on_disk("./bqfs/rdm_resize_done" + std::to_string(resize.quotient_size));
+    // mock.save_on_disk("./bqfs/rdm_mock_done" + std::to_string(mock.quotient_size));
+    std::cout << "All Done !" << std::endl;
 }
